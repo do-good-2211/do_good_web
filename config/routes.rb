47cd9f3ce41dc6bc183a97358f91f2785bcfec6b
Huy Phan <huyphan520@gmail.com>
2023-04-12 17:02:15 -0500

    features: Update routes
    Co-authored-by: Menyeart <menyeart1@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get "/", to: "good_deeds#index"
 
-get "/", to: "good_deeds#index"
+  get "/login", to: "good_deeds#login"
 
-get "/login", to: "good_deeds#login"
+  get "/random_acts", to: "random_acts#index"
 
-get "/random_acts", to: "random_acts#index"
+  get "/user/good_deeds/new", to: "good_deeds#new"
 end

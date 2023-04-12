dd59a8f7bd04a81e82d089c3584fc8f71fce5fce
Huy Phan <huyphan520@gmail.com>
2023-04-12 17:08:40 -0500

    features: Create facade,feature spec-random_act
    Co-authored-by: Menyeart <menyeart1@gmail.com>

---
+++ spec/facades/random_act_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+require "./app/facades/random_act_facade"
+
+RSpec.describe RandomActFacade do
+  before do
+    random_acts = File.read("./spec/fixtures/random_acts.json")
+    stub_request(:get, "https://localhost:3000/api/v1/random_acts")
+      .to_return(status: 200, body: random_acts)
+
+    @facade = RandomActFacade.new
+  end
+
+  describe "initialize" do
+    it "exists" do
+      expect(@facade).to be_instance_of(RandomActFacade)
+    end
+  end
+
+  describe "#methods" do
+    it "returns an array of random act objects and it name" do
+      acts = @facade.create_acts
+
+      expect(acts).to be_an(Array)
+      expect(acts.size).to eq(3)
+      expect(acts.first.name).to be_a(String)
+      expect(acts.first).to be_an_instance_of(RandomAct)
+    end
+  end
+end

ac347629c9f95bd7c8e84391bdbebcd6c5815c3f
Huy Phan <huyphan520@gmail.com>
2023-04-12 16:56:16 -0500

    features: Update new method
    Co-authored-by: Menyeart <menyeart1@gmail.com>

--- app/controllers/good_deeds_controller.rb
+++ app/controllers/good_deeds_controller.rb
@@ -3,7 +3,12 @@ class GoodDeedsController < ApplicationController
     @deeds = GoodDeedFacade.new
   end
 
-  def login
-    
+  def new
+    return unless current_user.nil?
+
+    redirect_to login_path
+    flash[:alert] = "You must be logged in to create a new good deed"
   end
+
+  def login; end
 end

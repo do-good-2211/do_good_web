829ade15e45758f335abf6a0d3661b3b1e193204
KaraJH <karas.jones@yahoo.com>
2023-04-13 16:52:08 -0600

    feat: Callback route for google oauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,12 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+end
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+#     {
+#       scope: 'email, profile',
+#       prompt: 'select_account',
+#     }
+# end
+OmniAuth.config.allowed_request_methods = %i[get]

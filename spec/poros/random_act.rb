6cc6d234c15438a862bc40ed9cb83f8b38d25707
Melony Franchini <meltravelz@gmail.com>
2023-04-14 16:27:32 -0600

    test: Create poro specs

--- spec/poros/random_act.rb
+++ spec/poros/random_act.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe RandomAct do 
   let(:random_act) { RandomAct.new("Pick or buy flowers for your mother.") }
 
+  describe 
   it "exists & has attributes" do
     expect(random_act).to be_a(RandomAct)
     expect(random_act.name).to eq("Pick or buy flowers for your mother.")
---
+++ spec/poros/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User do 
+  let(:info) { {:name=>"Peter Pan", :email=>"the_pan@neverland.com", :password_digest=>"NeverGrowUp", :role=>"User"} }
+  let(:peter) { User.new(info) }
+
+  it "exists & has attributes" do
+    expect(peter).to be_a(User)
+    expect(peter.name).to eq("Peter Pan")
+    expect(peter.email).to eq("the_pan@neverland.com")
+    expect(peter.password_digest).to eq("NeverGrowUp")
+    expect(peter.role).to eq("User")
+  end
+end

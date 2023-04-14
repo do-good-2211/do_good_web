e81712b5cb95484a61852da2cbe79530a1b2d8d0
KaraJH <karas.jones@yahoo.com>
2023-04-14 15:28:57 -0600

    test: Oauth logic spec

--- spec/features/login/index_spec.rb
+++ spec/features/login/index_spec.rb
@@ -1,25 +1,22 @@
 require "rails_helper"
 
-RSpec.describe "Login Page" do 
-  before do 
-    OmniAuth.config.mock_auth[:google_oauth2] = nil
-    
-    omni_hash
-    visit login_path
-    click_on "Log in with Google" 
-
-    # @controller.stub(:env).and_return({"omniauth.auth" => OmniAuth.config.mock_auth[:google_oauth2]})  
-    allow(@controller).to receive(:env).and_return({"omniauth.auth" => OmniAuth.config.mock_auth[:google_oauth2]})
-
+RSpec.describe "Login Page" do
+  describe "when I visit the login page", :vcr do
+    it " has a link to login with google" do
+      expect(page).to have_link("Log in with Google")
+    end
   end
 
-  describe "when I visit the login page", :vcr do 
-    # xit" has a link to login with google" do 
-    #   expect(page).to have_link("Log in with Google")
-    # end
+  before do
+    OmniAuth.config.mock_auth[:google_oauth2] = nil
 
-    it "takes me to log in with my google credentials" do 
-      # click_on "Log in with Google"
+    omni_hash
+    visit login_path
+    click_on "Log in with Google"
+  end
+
+  describe "when I visit the login page", :vcr do
+    it "takes me to log in with my google credentials" do
       expect(current_path).to eq("/login")
       expect(page).to have_content("Welcome, John Smith!")
     end

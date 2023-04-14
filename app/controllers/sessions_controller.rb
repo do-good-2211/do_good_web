cd4f301c7cccc84a48848443a6f2de247d57a6dd
KaraJH <karas.jones@yahoo.com>
2023-04-14 15:32:02 -0600

    feat: Refactor omniauth controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
-class SessionsController < ApplicationController 
-  def omniauth 
-    # require 'pry'; binding.pry
-    user_response = DoGoodService.create_user(request.env["omniauth.auth"])
-    user = User.new(user_response[:data])
-    session[:user_id] = user.id 
+# frozen_string_literal: true
+
+# app/controllers/sessions_controller.rb
+class SessionsController < ApplicationController
+  def omniauth
+    user = SessionsFacade.new.authorize_user(request.env["omniauth.auth"])
+    session[:user_id] = user.id
     flash[:message] = "Welcome, #{user.name}!"
-    
-    
+
     redirect_to "/login"
   end
 end

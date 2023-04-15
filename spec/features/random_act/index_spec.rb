7d525b85858133e9a7c277937b4d2d8492b66840
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-15 16:51:18 -0700

    fix: update tests to use vcr

--- spec/features/random_act/index_spec.rb
+++ spec/features/random_act/index_spec.rb
@@ -1,28 +1,26 @@
 require "rails_helper"
 
 RSpec.describe "Random Acts index page", type: :feature do
-  describe "As a visitor", :vcr do
+  describe "When I visit '/random_acts'", :vcr do
     before do
       VCR.use_cassette('random_acts', serialize_with: :json) do
-        @random_acts = RandomActFacade.new.create_acts
+        @random_acts = ["Contribute code or a monetary donation to an open-source software project", "Contribute code or a monetary donation to an open-source software project", "Clean out your closet and donate the clothes you've outgrown"]
 
         visit '/random_acts'
       end
+
+      user = {id: 1, attributes: { name: "Bob", email: "user@example.com", password_digest: "test1", role: "User" }}
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     end
 
-    it "When I visit '/random_acts' I see 3 random acts and their name as a link" do
-      expect(page).to have_link(@random_acts.first.name)
-      expect(page).to have_link(@random_acts.second.name)
-      expect(page).to have_link(@random_acts.last.name)
+    it "I see 3 random acts and their name as a link" do
+      expect(page).to have_link(@random_acts.first)
+      expect(page).to have_link(@random_acts.second)
+      expect(page).to have_link(@random_acts.last)
     end
 
     it "As a logged-in User, when I click on any deed I am redirected to the new good deed page" do
-      user = User.new(id: 1, attributes: { name: "Bob", email: "user@example.com", password_digest: "test1", role: "User" })
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-      visit '/random_acts'
-
-      click_on(@random_acts.first.name)
+      click_on(@random_acts.first, match: :first)
 
       expect(current_path).to eq("/user/good_deeds/new")
     end

d148c44e26ed9d8a20ea40c6b69b93537a6435a9
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-15 11:14:12 -0700

    test: create new good deed test

--- spec/features/users/good_deeds/new_spec.rb
+++ spec/features/users/good_deeds/new_spec.rb
@@ -1,24 +1,27 @@
 require "rails_helper"
 
-RSpec.describe "User good deed new page" do
-  describe "As a logged in user" do
+RSpec.describe "User good deed new page", type: :feature do
+  describe "As a logged in user", :vcr do
     before do
-      user = File.read("./spec/fixtures/user.json")
-      stub_request(:get, "https://localhost:3000/api/v1/users/1")
-      .to_return(status: 200, body: user)
-  
-      all_users = File.read("./spec/fixtures/users.json")
-      stub_request(:get, "https://localhost:3000/api/v1/users")
-      .to_return(status: 200, body: all_users)
-
-      info = { attributes:{name: "Bob"} }
-      user = User.new(info)
+      user = User.new(id: 1, attributes: { name: "Bob", email: "user@example.com", password_digest: "test1", role: "User" })
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      VCR.use_cassette('random_acts', serialize_with: :json) do
+        @random_acts = RandomActFacade.new.create_acts
+        @users = UserFacade.new({good_deed: @random_acts.first.name}).fetch_all_users
+        
+        visit random_acts_path
+
+        click_link @random_acts.first
+      end
     end
 
-    xit "When I visit '/user/good_deeds/new' I see the name of the good deed, a from with fields for date, time and checkboxes for other users and a button 'Do Good'" do
-      visit 'user_good_deeds_new_path'
-      expect(page).to have_content()
+    xit "When I visit '/user/good_deeds/new' I see the name of the good deed, a form with fields for date, time and checkboxes for other users and a button 'Do Good'" do
+      expect(page).to have_content(@random_acts.first.name)
+      expect(page).to have_field(:date)
+      expect(page).to have_field(:time)
+      expect(page).to have_unchecked_field (@users.first.name)
+      expect(page).to have_uncheckted
     end  
   end
 end

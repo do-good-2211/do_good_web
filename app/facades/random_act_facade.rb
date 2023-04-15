d1f2b63c93251d7afe46a64faacf1e6d42d39ce1
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-15 08:09:18 -0700

    style: add string literal and path, remove empty initialize

--- app/facades/random_act_facade.rb
+++ app/facades/random_act_facade.rb
@@ -1,8 +1,8 @@
+# frozen_string_literal: true
+
+# ./app/facades/random_act_facade.rb
 class RandomActFacade
-  def initialize; end
-
   def create_acts
-
     RandomActService.get_acts[:data][:attributes][:deed_names].map do |act|
       RandomAct.new(act)
     end

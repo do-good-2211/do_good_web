b0136123bbbb24ee09c1a0edc5c1c4b26cc295fc
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-15 16:48:45 -0700

    fix: change controller for root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root 'user/good_deeds#index'
+  root 'good_deeds#index'
 
   get "/login", to: "users#login"
   get "/auth/:provider/callback", to: "sessions#omniauth"
   get "/dashboard", to: "users#show"
 
   namespace :user do
-    resources :good_deeds, only: [:new]
+    resources :good_deeds, only: [:new, :create]
   end
 
   resources :random_acts, only: [:index]

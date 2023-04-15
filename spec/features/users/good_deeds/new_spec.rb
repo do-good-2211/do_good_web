343372c27e7c87ec6a6a0cff6fa4c9c8f20b5f7d
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-15 16:54:20 -0700

    test: create happy path tests for new good deed page

--- spec/features/users/good_deeds/new_spec.rb
+++ spec/features/users/good_deeds/new_spec.rb
@@ -1,27 +1,44 @@
 require "rails_helper"
 
-RSpec.describe "User good deed new page", type: :feature do
+RSpec.describe "User good deed new page" do
   describe "As a logged in user", :vcr do
-    before do
-      user = User.new(id: 1, attributes: { name: "Bob", email: "user@example.com", password_digest: "test1", role: "User" })
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    context "When I visit '/user/good_deeds/new" do
+      before do
+        @user = {id: 1, attributes: { name: "Bob", email: "user@example.com", password_digest: "test1", role: "User" }}
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-      VCR.use_cassette('random_acts', serialize_with: :json) do
-        @random_acts = RandomActFacade.new.create_acts
-        @users = UserFacade.new({good_deed: @random_acts.first.name}).fetch_all_users
-        
-        visit random_acts_path
+        VCR.use_cassette('random_acts', serialize_with: :json) do
+          visit random_acts_path
 
-        click_link @random_acts.first
+          click_link "Contribute code or a monetary donation to an open-source software project", match: :first
+        end
+
+        @users = UserFacade.new({good_deed: "Contribute code or a monetary donation to an open-source software project"}, @user).fetch_all_but_user
+      end
+
+      it "I see the name of the good deed, a form with fields for date, time and checkboxes for other users and a button 'Do Good'" do
+        expect(page).to have_content('Contribute code or a monetary donation to an open-source software project')
+
+        within '#new_good_deed' do
+          expect(page).to have_field(:date)
+          expect(page).to have_field(:time)
+          expect(page).to have_content(@users.first.name)
+          expect(page).to have_button("Create Good!")
+        end
+      end
+    
+      it 'When I fill in all part of the form and click "Create Good!" I am taken to my user dashboard' do
+        within '#new_good_deed' do
+          fill_in :date, with: Date.today
+          fill_in :time, with: Time.now
+          
+          find(:css, "#attendees_#{@users.first.id}").set true
+          find(:css, "#attendees_#{@users.last.id}").set true
+          click_button 'Create Good!'
+
+          expect(current_path).to eq(dashboard_path)
+        end
       end
     end
-
-    xit "When I visit '/user/good_deeds/new' I see the name of the good deed, a form with fields for date, time and checkboxes for other users and a button 'Do Good'" do
-      expect(page).to have_content(@random_acts.first.name)
-      expect(page).to have_field(:date)
-      expect(page).to have_field(:time)
-      expect(page).to have_unchecked_field (@users.first.name)
-      expect(page).to have_uncheckted
-    end  
   end
 end

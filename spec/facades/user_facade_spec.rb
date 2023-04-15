257a9984fd868f54fcfda4f03cf8c2aff04b7587
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-15 16:51:38 -0700

    test: update test to reflect changes in facade

--- spec/facades/user_facade_spec.rb
+++ spec/facades/user_facade_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe UserFacade, :vcr do
   before do
-    @facade = UserFacade.new({})
+    @facade = UserFacade.new({}, {id: 2, attributes: { name: "John Smith", role: "User" }})
   end
 
   describe "initialize" do
@@ -11,10 +11,9 @@ RSpec.describe UserFacade, :vcr do
     end
   end
 
-  describe "#create_user" do
+  describe "#fetch_user" do
     it "returns one user object with its attributes" do
-      user_id = 1
-      user_1 = @facade.fetch_user(user_id)
+      user_1 = @facade.fetch_user
 
       expect(user_1).to be_an_instance_of(User)
       expect(user_1.name).to be_a(String)
@@ -22,9 +21,21 @@ RSpec.describe UserFacade, :vcr do
     end
   end
 
-  describe "create_all_users" do
+  describe "fetch_all_users" do
     it "returns all user objects with their respective attributes" do
       users = @facade.fetch_all_users
+
+      users.each do |user|
+        expect(user).to be_an_instance_of(User)
+        expect(user.name).to be_a(String)
+        expect(user.id).to be_a(String)
+      end
+    end
+  end
+
+  describe "fetch_all_but_user" do
+    it "returns all users excluding the designated user" do
+      users = @facade.fetch_all_but_user
 
       users.each do |user|
         expect(user).to be_an_instance_of(User)

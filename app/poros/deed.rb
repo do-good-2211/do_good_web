7a5a25696b04172c8b2bd2c2e0956049ca0e8626
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-16 10:56:05 -0700

    feat: add additional attributes to deed poro

--- app/poros/deed.rb
+++ app/poros/deed.rb
@@ -2,13 +2,21 @@
 
 # app/poros/deed.rb
 class Deed
-  attr_reader :name,
-              :media_link,
-              :id
+  attr_reader :id,
+              :name,
+              :date,
+              :time,
+              :status,
+              :notes,
+              :media_link
 
   def initialize(info)
     @id = info[:id].to_i
     @name = info[:attributes][:name]
+    @date = info[:attributes][:date]
+    @time = info[:attributes][:time].to_datetime.strftime("%l:%M %p").strip
+    @status = info[:attributes][:status]
+    @notes = info[:attributes][:notes]
     @media_link = info[:attributes][:media_link]
   end
 end

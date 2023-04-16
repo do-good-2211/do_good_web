eb91e3ff5ec121239f400b983559ed41d9e1645b
Matt Enyeart <menyeart1@gmail.com>
2023-04-16 17:20:26 -0600

    chore: fix spacing

--- app/controllers/good_deeds_controller.rb
+++ app/controllers/good_deeds_controller.rb
@@ -5,8 +5,4 @@ class GoodDeedsController < ApplicationController
   def index
     @deeds = GoodDeedFacade.new(params, nil)
   end
-
-  def create
-     # Need to pass the users token (session[:token]), email (from params or from finding a user object) and create params. If created successfully, add it to the calendar.
-  end
 end

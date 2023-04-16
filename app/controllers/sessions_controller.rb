0ada7b42a072352ac2035216ca991ecb8ed29c89
Matt Enyeart <menyeart1@gmail.com>
2023-04-16 17:21:54 -0600

    refactor:move calendar logic to respective facade and service

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,33 +5,10 @@ class SessionsController < ApplicationController
   def omniauth
     user = SessionsFacade.new.authorize_user(request.env["omniauth.auth"])
     session[:user] = user
-   
+    session[:token] = request.env["omniauth.auth"][:credentials][:token]
     if current_user
       redirect_to "/dashboard"
     end
-  
-    session[:token] = request.env["omniauth.auth"][:credentials][:token]
-
-    service = Google::Apis::CalendarV3::CalendarService.new
-
-    service.authorization = session[:token]
-
-    s = DateTime.new(2023, 12, 9, 12, 0, 0)
-    e = DateTime.new(2023, 12, 9, 13, 0, 0)
- 
-    event= Google::Apis::CalendarV3::Event.new(
-        summary: "Help people", 
-        description: "Help people more",
-        start: {date: "2023-07-30", dateTime: '2023-05-29T13:15:03Z', timeZone: 'local'},
-        end: {date: "2023-07-30", dateTime: '2023-05-29T13:15:03Z', timeZone: 'local'}
-    )
-
-    service.insert_event("menyeart1@gmail.com", event)
-
-    # Listing Calendar On A Page
-    # @calendar_list = service.list_calendar_lists.items  
-
-
   end
 
   def logout

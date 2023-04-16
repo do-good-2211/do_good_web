6b10a77dacfa57af26e9d4da96bb29d5acd9736f
Melony Franchini <meltravelz@gmail.com>
2023-04-16 15:46:06 -0600

    fix: update homepage spec after Tailwind

--- spec/features/homepage/index_spec.rb
+++ spec/features/homepage/index_spec.rb
@@ -15,24 +15,32 @@ RSpec.describe "The good deeds index page" do
     it "When I visit '/' I see the good deed index info" do
       visit "/"
 
-      expect(page).to have_content("Do Good")
+      expect(page).to have_link("Home")
+      expect(page).to have_have_css('img[src^="/assets/do_good.jpg"][alt="Do Good Logo"][class="mx-auto w-1/5 h-auto"]')
       expect(page).to have_content("Ok, I'm Convinced. I wanna join the fun")
-      expect(page).to have_link("Login/Register")
-      expect(page).to have_button("Random Acts")
+      expect(page).to have_button("Login/Register")
+      expect(page).to have_link("Get Help Today")
     end
 
-    it "When I click on Login/Register I am redirected" do
+    it "When I click on Login/Register button I am redirected" do
       visit "/"
-
       click_on "Login/Register"
+
       expect(current_path).to eq("/login")
     end
 
-    it "When I click on Random Act button I am redirected" do
+    it "When I click on Home linkI am redirected to Home" do
       visit "/"
+      click_on "Home"
 
-      click_on "Random Act"
-      expect(current_path).to eq("/random_acts")
+      expect(current_path).to eq("/")
+    end
+
+    it "When I click on Get Help Now link I am redirected to a helpful link" do
+      visit "/"
+      click_on "Get Help Today"
+
+      expect(current_path).to eq("https://www.mentalhealth.gov/get-help/immediate-help")
     end
   end
 end

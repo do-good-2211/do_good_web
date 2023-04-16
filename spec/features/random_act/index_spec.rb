d56d3cf6ce00f863b85d938e9b432129be632bad
KaraJH <karas.jones@yahoo.com>
2023-04-16 13:14:22 -0600

    test/fix: Adjust tests to have a logged in user to view their pages

--- spec/features/random_act/index_spec.rb
+++ spec/features/random_act/index_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 RSpec.describe "Random Acts index page", type: :feature do
   describe "When I visit '/random_acts'", :vcr do
     before do
+      user = { id: 1, attributes: { name: "Bob", email: "user@example.com", password_digest: "test1", role: "User" } }
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       VCR.use_cassette('random_acts', serialize_with: :json) do
         @random_acts = ["Contribute code or a monetary donation to an open-source software project", "Contribute code or a monetary donation to an open-source software project", "Clean out your closet and donate the clothes you've outgrown"]
 
         visit '/random_acts'
       end
-
-      user = { id: 1, attributes: { name: "Bob", email: "user@example.com", password_digest: "test1", role: "User" } }
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     end
 
     it "I see 3 random acts and their name as a link" do

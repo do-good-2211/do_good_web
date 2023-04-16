64f7be77f9dbb7f7e56f45cf4b20fc5344031ac1
Matt Enyeart <menyeart1@gmail.com>
2023-04-15 18:08:43 -0600

    feat: google calendar scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], 
+  { 
+    scope: "userinfo.email, calendar" 
+
+  }
 end
 OmniAuth.config.allowed_request_methods = %i[get]

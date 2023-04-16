c486b2d91716b59ec174abd3fa655af348bb1306
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-16 10:57:43 -0700

    test: create test for #fetch_deed()

--- spec/services/good_deed_service_spec.rb
+++ spec/services/good_deed_service_spec.rb
@@ -2,18 +2,46 @@ require "rails_helper"
 
 RSpec.describe "The good deeds service", :vcr do
   describe "Class Methods" do
-    it "returns a json object" do
-      data = GoodDeedService.good_deeds
+    describe '#good_deeds' do
+      it "returns a json object" do
+        data = GoodDeedService.good_deeds
 
-      expect(data).to be_a(Hash)
-      data[:data].each do |deed|
-        expect(deed).to have_key(:id)
-        expect(deed).to have_key(:type)
-        expect(deed).to have_key(:attributes)
-        expect(deed[:attributes]).to have_key(:deed_name)
-        expect(deed[:attributes][:deed_name]).to be_a(String)
-        expect(deed[:attributes]).to have_key(:media_link)
-        expect(deed[:attributes][:media_link]).to be_a(String)
+        expect(data).to be_a(Hash)
+
+        data[:data].each do |deed|
+          expect(deed).to have_key(:id)
+          expect(deed).to have_key(:type)
+          expect(deed).to have_key(:attributes)
+          expect(deed[:attributes]).to have_key(:name)
+          expect(deed[:attributes][:deed_name]).to be_a(String)
+          expect(deed[:attributes]).to have_key(:media_link)
+          expect(deed[:attributes][:media_link]).to be_a(String)
+        end
+      end
+    end
+
+    describe '#fetch_deed()' do
+      it 'returns a single deed and its attributes' do
+        data = GoodDeedService.fetch_deed(1, 1)
+
+        expect(data).to be_a(Hash)
+
+        deed = data[:data]
+          expect(deed).to have_key(:id)
+          expect(deed).to have_key(:type)
+          expect(deed).to have_key(:attributes)
+          expect(deed[:attributes]).to have_key(:name)
+          expect(deed[:attributes][:name]).to be_a(String)
+          expect(deed[:attributes]).to have_key(:date)
+          expect(deed[:attributes][:date]).to be_a(String)
+          expect(deed[:attributes]).to have_key(:time)
+          expect(deed[:attributes][:time]).to be_a(String)
+          expect(deed[:attributes]).to have_key(:status)
+          expect(deed[:attributes][:status]).to be_a(String)
+          expect(deed[:attributes]).to have_key(:notes)
+          expect(deed[:attributes][:notes]).to be_a(String).or eq(nil)
+          expect(deed[:attributes]).to have_key(:media_link)
+          expect(deed[:attributes][:media_link]).to be_a(String).or eq(nil)
       end
     end
   end

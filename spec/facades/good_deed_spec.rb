86705854519e5a2f08b1b0f65d9a2049bbf94b47
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-16 11:00:14 -0700

    test: create test for #fetch_deed facade method

--- spec/facades/good_deed_spec.rb
+++ spec/facades/good_deed_spec.rb
@@ -3,7 +3,8 @@ require "rails_helper"
 RSpec.describe GoodDeedFacade, :vcr do
   before do
     @facade = GoodDeedFacade.new({
-                                   name: "High-five a stranger.",
+                                  id: 1, 
+                                  name: "High-five a stranger.",
                                    date: "02-02-2024",
                                    time: "2000-01-01T16:00:00.000Z",
                                    attendees: [1]
@@ -37,6 +38,21 @@ RSpec.describe GoodDeedFacade, :vcr do
       expect(good_deed).to be_a Deed
       expect(good_deed.id).to be_an Integer
       expect(good_deed.name).to be_a String
+    end
+  end
+
+  describe "#fetch_deed" do
+    it 'returns a good deed object with its respective attributes' do
+      good_deed = @facade.fetch_deed
+
+      expect(good_deed).to be_a Deed
+      expect(good_deed.id).to be_an Integer
+      expect(good_deed.name).to be_a String
+      expect(good_deed.date).to be_a String
+      expect(good_deed.time).to be_a String
+      expect(good_deed.status).to be_a String
+      expect(good_deed.notes).to be_a(String).or eq(nil)
+      expect(good_deed.media_link).to be_a(String).or eq(nil)
     end
   end
 end

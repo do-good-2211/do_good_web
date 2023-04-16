2f895eb1ce71abbf13243fc8b78faba6f66fd6bf
Matt Enyeart <menyeart1@gmail.com>
2023-04-16 17:22:58 -0600

    refactor: add additional logic to calendar facade

--- app/facades/calendar_facade.rb
+++ app/facades/calendar_facade.rb
@@ -3,18 +3,21 @@ class CalendarFacade
   end
 
   def create_event(email, attributes, token)
-    CalendarService.calendar_api(token)
+    service = CalendarService.calendar_api
+    service.authorization = token
 
-    event= Google::Apis::CalendarV3::Event.new(
-      summary: "Date: #{params[date]}: Do Good Event!", 
-      description: "#{params[:name]} at #{params[:time]} with #{params[:attendees].count} of your friends!",
-      start: {date: "2023-07-30"},
-      end: {date: "2023-07-30"}
+    event = Google::Apis::CalendarV3::Event.new(
+      summary: "Do Good Event! Date: #{attributes[:date]}", 
+      description: "#{attributes[:name]} at #{attributes[:time]}!",
+      start: {date: attributes[:date]},
+      end: {date: attributes[:date]}
     )
     service.insert_event(email, event)
   end
 
   def list_calendars(email, token)
-    CalendarService.calendar_api(token).list_calendar_lists.items
+    service = CalendarService.calendar_api
+    service.authorization = token
+    service.get_calendar('primary')
   end
 end

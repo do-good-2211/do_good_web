910437f00317b79fad447e7a6bd1a63c0ba036fe
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-17 03:22:09 -0700

    test: create test for #update_deed

--- spec/services/do_good_service_spec.rb
+++ spec/services/do_good_service_spec.rb
@@ -13,23 +13,48 @@ RSpec.describe DoGoodService, :vcr do
     end
   end
 
-  it "returns a json object of a good deed" do
-    good_deed = DoGoodService.create_deed(2, {
-                                            name: "High-five a stranger.",
-                                            date: "02-02-2024",
-                                            time: "2000-01-01T16:00:00.000Z",
-                                            attendees: [1]
-                                          })
+  describe '#create_deed' do
+    it "returns a json object of a created good deed" do
+      good_deed = DoGoodService.create_deed(2, {
+                                              name: "High-five a stranger.",
+                                              date: "02-02-2024",
+                                              time: "2000-01-01T16:00:00.000Z",
+                                              attendees: [1]
+                                            })
 
-    expect(good_deed).to be_a(Hash)
+      expect(good_deed).to be_a(Hash)
 
-    expect(good_deed[:data]).to have_key(:id)
-    expect(good_deed[:data]).to have_key(:type)
-    expect(good_deed[:data]).to have_key(:attributes)
-    expect(good_deed[:data][:attributes]).to have_key(:name)
-    expect(good_deed[:data][:attributes]).to have_key(:host_id)
-    expect(good_deed[:data][:attributes]).to have_key(:date)
-    expect(good_deed[:data][:attributes]).to have_key(:time)
-    expect(good_deed[:data][:attributes]).to have_key(:status)
+      expect(good_deed[:data]).to have_key(:id)
+      expect(good_deed[:data]).to have_key(:type)
+      expect(good_deed[:data]).to have_key(:attributes)
+      expect(good_deed[:data][:attributes]).to have_key(:name)
+      expect(good_deed[:data][:attributes]).to have_key(:host_id)
+      expect(good_deed[:data][:attributes]).to have_key(:date)
+      expect(good_deed[:data][:attributes]).to have_key(:time)
+      expect(good_deed[:data][:attributes]).to have_key(:status)
+    end
+  end
+
+  describe '#updated_deed' do
+    it "returns a json object of an updated good deed" do
+      good_deed = DoGoodService.update_deed(2, 5, {
+                                              name: "High-five a stranger.",
+                                              date: "02-02-2024",
+                                              time: "2000-01-01T16:00:00.000Z",
+                                              attendees: [1],
+                                              notes: "This was awesome"
+                                            })
+
+      expect(good_deed).to be_a(Hash)
+
+      expect(good_deed[:data]).to have_key(:id)
+      expect(good_deed[:data]).to have_key(:type)
+      expect(good_deed[:data]).to have_key(:attributes)
+      expect(good_deed[:data][:attributes]).to have_key(:name)
+      expect(good_deed[:data][:attributes]).to have_key(:date)
+      expect(good_deed[:data][:attributes]).to have_key(:time)
+      expect(good_deed[:data][:attributes]).to have_key(:status)
+      expect(good_deed[:data][:attributes]).to have_key(:notes)
+    end
   end
 end

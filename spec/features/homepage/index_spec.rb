30bb835194f6ec1a96ab345578abd07d0852a609
Matt Enyeart <menyeart1@gmail.com>
2023-04-17 22:11:01 -0600

    test: update mocks

--- spec/features/homepage/index_spec.rb
+++ spec/features/homepage/index_spec.rb
@@ -5,58 +5,33 @@ RSpec.describe "Home page", type: :feature do
 
   describe "As a user who is NOT logged in", :vcr do
     it "When I visit '/' I see a link to home, a button to login/register, logo and moto, and footer" do
-      visit "/"
-  describe "As a user who is NOT logged in", :vcr do
-    it "When I visit '/' I see a link to home, a button to login/register, logo and moto, and footer" do
-      visit "/"
-
-      expect(page).to have_link("Home")
-      expect(page).to have_css("img[src*='do_good']")
-      expect(page).to have_content("Ok, I'm Convinced. I wanna join the fun")
-      expect(page).to have_link("Login/Register")
-      expect(page).to have_link("Get Help Today", href: "https://www.mentalhealth.gov/get-help/immediate-help")
-    end
-      expect(page).to have_link("Home")
-      expect(page).to have_css("img[src*='do_good']")
-      expect(page).to have_content("Ok, I'm Convinced. I wanna join the fun")
-      expect(page).to have_link("Login/Register")
-      expect(page).to have_link("Get Help Today", href: "https://www.mentalhealth.gov/get-help/immediate-help")
-    end
-
-    it "When I click on Login/Register button I am redirected" do
-      visit "/"
-    it "When I click on Login/Register button I am redirected" do
-      visit "/"
-
-      click_button("Login/Register")
-      # expect(current_path).to eq(login_path) -> Now goes to Google OAuth
-      # Since this tests passes it's still being tested? If you have ideas on an expect statement please add.
-    end
-      click_button("Login/Register")
-      # expect(current_path).to eq(login_path) -> Now goes to Google OAuth
-      # Since this tests passes it's still being tested? If you have ideas on an expect statement please add.
-    end
-
-    it "When I click on Home link I am redirected to Home" do
-      visit "/"
-      click_on "Home"
-    it "When I click on Home link I am redirected to Home" do
-      visit "/"
-      click_on "Home"
-
-      expect(current_path).to eq("/")
-    end
-  end
-      expect(current_path).to eq("/")
-    end
-  end
-
-  describe "As a logged in user", :vcr do
-    it "should have a link to Log Out and My Page and Home" do
       user = User.new(id: 1, attributes: { name: "Bob", email: "user@example.com", password_digest: "test1", role: "User" })
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit "/"
 
-      visit root_path
+      expect(page).to have_link("Home")
+      expect(page).to have_css("img[src*='do_good']")
+      expect(page).to have_content("Ok, I'm Convinced. I wanna join the fun")
+      expect(page).to have_link("Login/Register")
+      expect(page).to have_link("Get Help Today", href: "https://www.mentalhealth.gov/get-help/immediate-help")
+    end
+
+    it "When I click on Login/Register button I am redirected" do
+      visit "/"
+
+      click_button("Login/Register")
+      # expect(current_path).to eq(login_path) -> Now goes to Google OAuth
+      # Since this tests passes it's still being tested? If you have ideas on an expect statement please add.
+    end
+
+    it "When I click on Home link I am redirected to Home" do
+      visit "/"
+      click_on "Home"
+
+      expect(current_path).to eq("/")
+    end
+  end
+
   describe "As a logged in user", :vcr do
     it "should have a link to Log Out and My Page and Home" do
       user = User.new(id: 1, attributes: { name: "Bob", email: "user@example.com", password_digest: "test1", role: "User" })
@@ -80,43 +55,20 @@ RSpec.describe "Home page", type: :feature do
 
     #   visit root_path
     #   click_on("Log Out")
-      expect(page).to have_link("Home")
-      expect(page).to have_link("Log Out")
-      expect(page).to have_link("My Page")
-    end
 
-    # Still unsure how to stub user login
-    # it "when I click log out, I am redirected to the homepage" do
-    #   # user = User.new(id: 1, attributes: { name: "Bob", email: "user@example.com", password_digest: "test1", role: "User" })
-    #   # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    #   OmniAuth.config.mock_auth[:google_oauth2] = nil
-    #   omni_hash
-    #   visit root_path
-    #   click_button("Login/Register")
-
-    #   visit root_path
-    #   click_on("Log Out")
-
-    #   expect(page).to_not have_content("Log Out")
-    #   expect(page).to_not have_content("My Page")
-    #   expect(page).to have_content("Successfully logged out")
-    # end
     #   expect(page).to_not have_content("Log Out")
     #   expect(page).to_not have_content("My Page")
     #   expect(page).to have_content("Successfully logged out")
     # end
 
     it "has a link to My Page and redirects me to my dashboard" do
+      user = { "id": 1, "name"=>"Bob", "email" => "user@gmail.com", "password_digest" => "test1", "role" => "User" }
       allow_any_instance_of(CalendarFacade).to receive(:list_events).and_return(12)
-      user = { "id" => "1", "name" => "Bob", "email" => "user@gmail.com", "password_digest" => "test1", "role" => "User" }
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit root_path
       click_on("My Page")
 
-      expect(current_path).to eq(dashboard_path)
-    end
-  end
       expect(current_path).to eq(dashboard_path)
     end
   end
